28138731ebde80e53f27a14b4a19996c6e708107
kibe <kibe@kibeMacbookAir.local>
2019-09-04 11:10:48 +0900

    Update SetDAQ.py

--- src/SetDAQ.py
+++ src/SetDAQ.py
@@ -36,10 +36,14 @@ class SetDAQ:
         pass
 
     def RunDAQMode(self):
-        Sig_DAQMode = TransferTCP.PackTCP(HeaderW, Addr_DAQ, DAQMode)
+        Sig_DAQMode = TransferTCP.PackTCP(HeaderW, 0x0, DAQMode)
         
         Nsnt = self.sock.SendTCP(Sig_DAQMode)
         Recv_Sig_DAQMode = self.sock.RecvTCP(Nsnt)
+
+        #for debug
+        #tmp = struct.unpack('I', Recv_Sig_DAQMode)
+        #print format(tmp[0], '08x')
         
         if(Sig_DAQMode and Recv_Sig_DAQMode):
             logger.info('Set Slow Control Mode')
@@ -67,6 +71,10 @@ class SetDAQ:
         
         Nsnt = self.sock.SendTCP(Sig_ModeSelect)
         Recv_Sig_ModeSelect = self.sock.RecvTCP(Nsnt)
+
+        #for debug
+        #tmp = struct.unpack('I', Recv_Sig_ModeSelect)
+        #print format(tmp[0], '08x')
         
         if(Sig_ModeSelect and Recv_Sig_ModeSelect):
             #logger.info('Set Slow Control Mode')
@@ -85,58 +93,45 @@ class SetDAQ:
             return -1
         
         #initialize variable
-        DataArray = [[] for i in xrange(4)]
+        #DataArray = [[] for i in xrange(4)]
+        DataArray = []
         
         #send signal to start data taking
-        Sig_DataTake = TransferTCP.PackTCP(HeaderW, Addr_DataTake, 0)
-        
+        Sig_DataTake = TransferTCP.PackTCP(HeaderStart, Addr_DataTake, 0)
+
+        #just only send because recv value is data
         Nsnt = self.sock.SendTCP(Sig_DataTake)
-        Recv_Sig_DataTake = self.sock.RecvTCP(Nsnt)
         
-        if(Sig_DataTake and Recv_Sig_DataTake):
-            print 'Start data taking......'
-        else:
-            logger.error('failure to send start daq signal!!')
-            return -1
-        
-        isErr = False
-        Nnow = 0
-        pbar = tqdm(total = int(1.005* NofData))
-        pbar.set_description('Progress of data taking')
-        
-        try:
-            #data taking
-            while(Nnow < NofData):
-
-                #read data from device
+        #data taking
+        for i in tqdm(xrange(NofData), desc = 'Data taking'):
+                
+            #read data from device
+            try:
                 tmpArray = self.ReadADC()
-                
-                if(tmpArray == None):
-                    isErr = True
-                    raise RuntimeError
-                
-                #fill read data into arrays
-                for i in xrange(len(DataArray)):
-                    DataArray[i] += tmpArray[i]
-                    pass
-                
-                Nnow += len(tmpArray[0])
-                pbar.update(len(tmpArray[0]))
-                
-        except KeyboardInterrupt:
+                DataArray.append(tmpArray)
+            except RuntimeError:
+                logger.info('fatal error is occrured')
+                print 'stop data taking by fatal error'
+                self.StopADC()
+            except KeyboardInterrupt:            
+                logger.info('keyborad interrupt(ctrl-c) is detected!!')
+                print 'stop data taking by ctrl-c'
+                self.StopADC()            
+
+            #fill read data into arrays
+            '''
+            for j in xrange(len(DataArray)):
+                DataArray[j] += tmpArray[j]
+                pass
+            '''
             
-            logger.info('keyborad interrupt(ctrl-c) is detected!!')
-            print 'stop data taking by ctrl-c'
-            return self.StopADC()            
-
-        pbar.close()
-        
-        if(isErr):
-            logger.error('error in read data!!')
-            return -1
-
+            pass                
+            
         print 'Finish to transfer data!!'
 
+        #end of adc
+        self.StopADC()
+        
         return DataArray
         
     def ReadADC(self):
@@ -150,20 +145,33 @@ class SetDAQ:
         #header of data
         headerADC = self._adc_header()
         
-        RecvHeader = self.sock.RecvTCP(len(headerADC))
-        RecvHeader = struct.unpack('3L', RecvHeader)
+        RecvHeader = self.sock.RecvTCP(len(headerADC))        
+        Header = struct.unpack('3I', RecvHeader)
 
+        #for debug
+        #for i in xrange(len(RecvHeader)):
+        #    tmp1 = (RecvHeader[i] & 0xffff0000) >> 16
+        #    tmp2 = (RecvHeader[i] & 0x0000ffff)
+        #    print format(tmp1, '04x')
+        #    print format(tmp2, '04x')
+        #    pass
+        
         #check abnormal header
-        if(RecvHeader[0] != 0xffffea0c):
+        if(Header[0] != 0xffffea0c):
             logger.error('abnormal header from data transfer!!')
             print format(RecvHeader[0], '08x')
             raise RuntimeError
         
         #get information from recieved header
-        NofWord = (RecvHeader[1] & 0xffff) #slice lower 16bit
+        #NofWord = (RecvHeader[1] & 0xffff) #slice lower 16bit
+        NofWord = 64 #slice lower 16bit, now fixed to be 64 words 
 
+        retry = 0
+        
         RecvData = self.sock.RecvTCP(4* NofWord)
-        RecvData = struct.unpack('%dL' %(NofWord), RecvData)
+
+        '''
+        RecvData = struct.unpack('%dI' %(NofWord), RecvData)
         
         #analyze data
         for i in xrange(len(RecvData)):
@@ -171,7 +179,7 @@ class SetDAQ:
             ChMPPC = (((RecvData[i] & 0x00ff0000) >> 16) & 0x1f)
             ADC = (RecvData[i] & 0x00000fff) 
             isOverflow = ((RecvData[i] & 0x00001fff) >> 12)
-
+            
             #fill variables into arrays
             ChipArray.append(ChipAB)
             ChArray.append(ChMPPC)
@@ -179,11 +187,13 @@ class SetDAQ:
             OFArray.append(isOverflow)
             
             pass
-
-        return (ChipArray, ChArray, ADCArray, OFArray)
+        '''
+        
+        #return (ChipArray, ChArray, ADCArray, OFArray)
+        return bytearray(RecvHeader + RecvData)
     
     def _adc_header(self, header1 = 1, header2 = 1, header3 = 1):
-        ret = struct.pack('3L',
+        ret = struct.pack('3I',
                           header1,
                           header2,
                           header3)
